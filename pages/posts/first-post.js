8f09b0cae3c2c91d0341812fd37ec201d6e86ff9
@umerwaseem99 <41803053+umerwaseem@users.noreply.github.com>
2022-06-06 16:03:08 +0500

    Add files via upload

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,115 +2,59 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import img from '../../public/images/home2.jpg'
-import img1 from '../../public/images/home2_1.jpeg'
-import img2 from '../../public/images/home2_2.jpg'
-import img3 from '../../public/images/home2_3.jpg'
-import img4 from '../../public/images/home2_4.jpg'
-import img5 from '../../public/images/home2_5.jpg'
-import Image from 'next/image'
+import Card from 'react-bootstrap/Card'
+import Image from "next/image";
+import Carousel from "react-bootstrap/Carousel";
+import Image1 from "../../public/images/carouselimg1.jpg";
+import Image2 from "../../public/images/carouselimg2.jpg";
+import Image3 from "../../public/images/carouselimg3.jpg";
+import Image4 from "../../public/images/carouselimg4.jpg";
+import { App, Code, Calendar, Tv } from "react-bootstrap-icons";
+import img from "../../public/images/home2.jpg";
+import img1 from "../../public/images/home2_1.jpeg";
+import img2 from "../../public/images/home2_2.jpg";
+import img3 from "../../public/images/home2_3.jpg";
+import img4 from "../../public/images/home2_4.jpg";
+import img5 from "../../public/images/home2_5.jpg";
+
 export default function FirstPost() {
   return (
-    <Container>
-    <Row className="my-5">
-      <Col> <h1 className="h1">MIDWAY</h1>
-              <h6 className="h6_1">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove.
-              </h6></Col>
-      <Col><Image
-        src={img}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      /></Col>
-    </Row>
-    <Row className="my-5">
-      <Col><Image
-        src={img1}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      /></Col>
-      <Col> <h1 className="h2">Workplace Office</h1>
-              <h6 className="h6_2">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove.
-              </h6></Col>
-    
-    </Row>
-    <Row className="my-5">
-      <Col><h1 className="h3">PRICKLY</h1>
-              <h6 className="h6_3">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove.
-              </h6></Col>
-      <Col><Image
-        src={img2}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      /></Col>
-    </Row>
-    <Row className="my-5">
-      <Col><Image
-        src={img3}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      /></Col>
-      <Col> <h1 className="h4">Topless</h1>
-              <h6 className="h6_4">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove.
-              </h6></Col>
-    
-    </Row>
-    <Row className="my-5">
-      <Col><h1 className="h5">Fashion Style</h1>
-              <h6 className="h6_5">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove.
-              </h6></Col>
-      <Col><Image
-        src={img4}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      /></Col>
-    </Row>
-    <Row className="my-5">
-      <Col><Image
-        src={img5}
-        alt="Picture of the author"
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      /></Col>
-      <Col>  <h1 className="h6">PRICKLY</h1>
-              <h6 className="h6_6">
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove.
-              </h6></Col>
-    
-    </Row>
-  </Container>
-  );
+    <div className='container'>
+    <Carousel className="carousel col-12">
+      <Carousel.Item className="caro">
+        <Image className="d-block w-100" src={Image1} alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="caro">
+        <Image className="d-block w-100" src={Image2} alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="caro">
+        <Image className="d-block w-100" src={Image3} alt="Second slide" />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="caro"> 
+        <Image className="d-block w-100" src={Image4} alt="Third slide" />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+ 
+  )
 }
